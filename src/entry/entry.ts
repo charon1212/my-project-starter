17b7d630011c3bd22066f19c43bf1d72cf8257bf
charon1212 <charons2turquoise@gmail.com>
2024-08-24 15:47:08 +0900

    add

--- src/entry/entry.ts
+++ src/entry/entry.ts
@@ -1,11 +1,14 @@
 import { mode_01_electron } from "../mode/01_electron/mode_01_electron";
+import { mode_03_nextjs } from "../mode/03_nextjs/mode_03_nextjs";
 import { mode_99_test } from "../mode/99_test/mode_99_test";
 import { menu } from "../util/menu";
 
 export const entry = async () => {
   const selectedMenu = await menu<() => unknown>([
     { id: 1, title: 'electron', desc: 'create typescript-electron project template.', value: mode_01_electron, },
+    { id: 3, title: 'nextjs', desc: 'create nextjs-react-typescript project template.', value: mode_03_nextjs, },
     { id: 99, title: 'test', desc: 'test for debug', value: mode_99_test, },
+    { id: 999, title: 'quit', desc: 'quit', value: () => { }, },
   ]);
   await selectedMenu.value();
 };
---
+++ src/mode/03_nextjs/mode_03_nextjs.ts
@@ -0,0 +1,18 @@
+import { command } from '../../util/command';
+import { logger } from '../../util/logger';
+
+export const mode_03_nextjs = async () => {
+
+  // create-next-appで、NextJSプロジェクトを構築する。
+  const commandCreateNextApp = "npx --yes create-next-app ." // --yesの指定で、常にパッケージのインストールに同意
+    + " --typescript"           // typescriptを利用する
+    + " --no-eslint"            // ESLintを利用しない
+    + " --no-tailwind"          // Tailwind CSSを利用しない
+    + " --src-dir"              // 「src/」ディレクトリを利用する
+    + " --app"                  // App Routerを利用する
+    + " --import-alias '@/*'";  // Import Aliasの設定（デフォルト設定）
+  logger.mark2(`[${commandCreateNextApp}] nextjsのテンプレートをインストール`);
+  const result1 = await command.exec(commandCreateNextApp);
+  if (result1.isEr) return;
+
+};
